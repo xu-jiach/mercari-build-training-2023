9b596b4891fe3a744535b703cc5bbe0009ffb7a5
xu-jiach <xu.jiach@northeastern.edu>
2023-05-18 11:31:37 +0900

    update the docker-compose.yml and minor changes to the Dokcerfile for the frontend

--- typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList/ItemList.tsx
@@ -46,14 +46,12 @@ export const ItemList: React.FC<Prop> = (props) => {
   }, [reload]);
 
   return (
-    <div className="Grid">
+    <div>
       {items.map((item) => {
-
-        const imageURL = server.concat('/images/', item.image_filename);
         return (
           <div key={item.id} className='ItemList'>
             {/* TODO: Task 1: Replace the placeholder image with the item image */}
-            <img className="ItemImage" src={imageURL} alt = {placeholderImage} />
+            <img src={placeholderImage} />
             <p>
               <span>Name: {item.name}</span>
               <br />
